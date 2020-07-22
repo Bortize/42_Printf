6bef13efe0b1375052a673264fe5345bc15622ac
Borja <bgomez-r@student.42madrid.com>
2020-07-22 13:03:33 +0200

    add flag_minus_zero.c flag_width.c

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/21 11:14:51 by bgomez-r          #+#    #+#             */
-/*   Updated: 2020/07/22 10:27:29 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2020/07/22 12:53:41 by bgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,14 @@ typedef struct	s_struct
 	va_list	ap;
 	int		i;
 	int		len;
+	int		flag_minus;
+	int		flag_zero;
+	int		flag_aux;
 }				t_struct;
 
 // ↓↓↓ FUNCTIONS PROTOIPES ↓↓↓
-
+int		ft_printf(const char *format, ...);
+int		check_flags(const char *format, t_struct *flags);
+void	flags_trigger(const char *format, t_struct *flags);
+void	flag_minus_zero(const char *format, t_struct *flags);
 #endif
