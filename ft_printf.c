a70b435e25747f2a01fae0fc5bf48c5a1b7fdddf
Borja <bgomez-r@student.42madrid.com>
2020-08-24 09:15:48 +0200

    remove space between the name function and the break's

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/21 11:34:12 by borjagrd          #+#    #+#             */
-/*   Updated: 2020/08/24 09:07:03 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2020/08/24 09:14:38 by bgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@
 
 int	ft_printf(const char *format, ...)
 {
-	t_struct    *flags;
+	t_struct	*flags;
 
 	if (!(flags = (t_struct *)malloc(sizeof(t_struct))))
 		return (0);
 	initialize_flags(flags);
 	if (format == NULL)
 		return (-1);
-	va_start (flags->ap, format);
+	va_start(flags->ap, format);
 	if (ft_strchr(format, '%'))
 		return (0);
 	else
 	{
 		ft_putstr_fd((char *)format, 1);
-		flags->len = ft_strlen (format);
+		flags->len = ft_strlen(format);
 	}
-	va_end (flags->ap);
+	va_end(flags->ap);
 	return (flags->len);
 }
