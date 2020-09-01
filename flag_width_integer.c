3df0e0b1402bc92b2b87e3a42dec39b1e9609e71
Borja <bgomez-r@student.42madrid.com>
2020-09-01 22:26:37 +0200

    add 'flags_width_integer' function

--- flag_width_integer.c
+++ flag_width_integer.c
@@ -6,7 +6,7 @@
 /*   By: borjagrd <borjagrd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/01 19:30:51 by borjagrd          #+#    #+#             */
-/*   Updated: 2020/09/01 20:57:08 by borjagrd         ###   ########.fr       */
+/*   Updated: 2020/09/01 22:25:55 by borjagrd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,25 @@ void    flag_width_integer(t_struct *flags, long int integer)
 {
     flags->j = 0;
     if (!(flags->flag_minus) && !(flags->flag_zero))
-        flags->count = 0;
-        
+    {
+        if (flags->flag_precision)
+        {
+            if (integer == 0 && flags->precision == 0)
+                flags->count = 0;
+            flags->aux = flags->precision > flags->count ? 
+            flags->precision : flags->count;
+            if ((flags->flag_integer_negative) && 
+            (flags->precision > flags->count))
+                flags->aux += 1;
+            if (flags->width > flags->aux)
+                print_integer_width(flags);
+        }
+        else
+            while (flags->j < (flasg->widht - flags->count))
+            {
+                write(1, " ", 1);
+                flags->len++;
+                flags->j++;
+            }
+    }
 }
