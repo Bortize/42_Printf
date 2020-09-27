d7e89870ce960f897576c907e21f88aa811c0633
Borja <bgomez-r@student.42madrid.com>
2020-09-28 00:40:44 +0200

    updates

---
+++ print_unsigned.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_unsigned.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgomez-r <bgomez-r@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/28 00:36:47 by bgomez-r          #+#    #+#             */
+/*   Updated: 2020/09/28 00:40:38 by bgomez-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include    "ft_printf.c"
+
+void    print_unsigned(t_struct *flags, unigned int unsign)
+{
+    char    *format;
+
+    flags->j = 0;
+    if (flags->)
+}
