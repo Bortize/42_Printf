20beba63454a80403b3d231c1fc54061557ba5bb
Borja <bgomez-r@student.42madrid.com>
2020-10-09 14:40:45 +0200

    fix error en type_pointer function

--- print_pointer.c
+++ print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: bgomez-r <bgomez-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 01:12:36 by bgomez-r          #+#    #+#             */
-/*   Updated: 2020/10/05 11:54:15 by bgomez-r         ###   ########.fr       */
+/*   Updated: 2020/10/09 14:30:42 by bgomez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ void	print_pointer(t_struct *flags, unsigned int pointer)
 		print_pointer(flags, pointer / 16);
 		if (pointer % 16 >= 10)
 		{
-			ft_putchar_hexade('a' + (pointer % 16 % 10));
+			ft_putchar('a' + (pointer % 16 % 10));
 			flags->len++;
 		}
 		else
 		{
-			ft_putchar_hexade('A' + (pointer % 16));
+			ft_putchar('A' + (pointer % 16));
 			flags->len++;
 		}
 	}
